2a5c3fdd46025f84a5ba746c938dcd6b3d904e60
anis <anissarker24@gmail.com>
2022-06-01 00:34:30 +0600

    crime commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Acme&family=Island+Moments&family=Pushster&family=The+Nautigal:wght@700&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+    />
+    <link
+      rel="shortcut icon"
+      href="image/36305450_2269312516621870_3738105767958413312_n.jpg"
+      type="image/x-icon"
+    />
+    <link rel="stylesheet" href="homepagecss\homePage.css"/>
+    <title>HomePage</title>
+  </head>
+  <body>
+    <nav>
+      <div class="log">
+        <h2>Development of Crime Record Management System</h2>
+      </div>
+      <ul class="menu">
+        <li><i class="bi bi-house"></i><a href="#">Home</a></li>
+        <li><i class="bi bi-patch-question"></i><a href="#">Registration</a></li>
+        <li><i class="bi bi-person-rolodex"></i><a href="">Login</a>
+          <ul>
+            <li><a href="#">Admin</a></li>
+            <li><a href="#">Police</a></li>
+            <li><a href="#">User</a></li>
+          </ul>
+        </li>
+        <li><i class="bi bi-patch-question"></i><a href="#">Contact us</a></li>
+      </ul>
+      <div class="toogle"><i class="bi bi-list"></i></div>
+    </nav>
+    </nav>
+    <h1>Thank You!</h1>
+    
+    <script src="custom.js"></script>
+  </body>
+</html>
